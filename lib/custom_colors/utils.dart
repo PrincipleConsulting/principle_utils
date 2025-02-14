ad4f18bc8e1656e43192503f201a5a17c3574106
CARLOS GARCIA <carlosgp196@gmail.com>
2025-02-14 00:03:43 -0600

    test/refactor: finished tests bug fix useWhiteForeground()

--- lib/custom_colors/utils.dart
+++ lib/custom_colors/utils.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 bool useWhiteForeground(Color backgroundColor, {double bias = 0.0}) {
-  int v = sqrt(pow(backgroundColor.r, 2) * 0.299 + pow(backgroundColor.g, 2) * 0.587 + pow(backgroundColor.b, 2) * 0.114).round();
-  return v < 130 + bias ? true : false;
+  final int v =
+      sqrt(pow(backgroundColor.r * 255, 2) * 0.299 + pow(backgroundColor.g * 255, 2) * 0.587 + pow(backgroundColor.b * 255, 2) * 0.114).round();
+  return v < (130 + bias);
 }
---
+++ test/platform_device/core/device_platform_test.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:principle_utils/platform_device/core/device_platform.dart';
+import 'package:principle_utils/platform_device/core/platform_type.dart';
+
+void main() {
+  group('DevicePlatformType (dart:io implementation)', () {
+    test('returns the proper platform type based on dart:io', () {
+      // Determine the expected platform type using dart:io's Platform properties.
+      PlatformType expected;
+      if (Platform.isWindows) {
+        expected = PlatformType.Windows;
+      } else if (Platform.isFuchsia) {
+        expected = PlatformType.Fuchsia;
+      } else if (Platform.isMacOS) {
+        expected = PlatformType.MacOS;
+      } else if (Platform.isLinux) {
+        expected = PlatformType.Linux;
+      } else if (Platform.isIOS) {
+        expected = PlatformType.IOS;
+      } else {
+        expected = PlatformType.Android;
+      }
+      // When running on non-web platforms, the currentPlatformType should match the expected value.
+      expect(DevicePlatformType.currentPlatformType, equals(expected));
+    });
+  });
+}
